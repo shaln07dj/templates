d6a75cb31d7a546972fbce4a47b6a827d0a3cdc3
ECHO-Shailendra <echo-shailendra@MacBook-Air.local>
2025-06-10 04:56:35 +0530

    template automate upated

--- convert_mjml.py
+++ convert_mjml.py
@@ -1,17 +1,19 @@
-import sys
-import os
+# import sys
+# import os
 
-# Add the parent directory of mjml_translate to the Python path
-# This allows importing mjml_translate from the project root
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+# # Add the parent directory of mjml_translate to the Python path
+# # This allows importing mjml_translate from the project root
+# sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from mjml_translate.translator import translate_changed_mjml_files
+# from mjml_translate.translator import translate_changed_mjml_files
 
-if __name__ == "__main__":
-    translated_files = translate_changed_mjml_files()
-    if translated_files:
-        print(f"Successfully translated and saved: {len(translated_files)} new files.")
-        for f in translated_files:
-            print(f"- {f}")
-    else:
-        print("No new translations generated.")
+# if __name__ == "__main__":
+#     translated_files = translate_changed_mjml_files()
+#     if translated_files:
+#         print(f"Successfully translated and saved: {len(translated_files)} new files.")
+#         for f in translated_files:
+#             print(f"- {f}")
+#     else:
+#         print("No new translations generated.")
+
+print("HELLO")
